6b9a5d73f8bdc1007baf92041205ac7e7329282f
Joe Wallace <joew@atiba.com>
2012-05-09 11:49:01 -0500

    Fixing a problem with tasks and auto_updating

--- start.php
+++ start.php
@@ -2,19 +2,23 @@
 
 $config = Config::get('composeur');
 
-$my_bundle = Request::cli() && function()
+$my_bundle = function()
 {
+	if (! Request::cli())
+		return false;
+
 	foreach ($_SERVER['argv'] as $key => $val)
 	{
 		if (strpos($val, 'composeur::') === 0)
 		{
+			echo 'my bundle';
 			return true;
 		}
 	}
 	return false;
 };
 
-if (! $my_bundle && isset($config['auto_update']) && $config['auto_update'])
+if (! $my_bundle() && isset($config['auto_update']) && $config['auto_update'])
 {
 	require dirname(__FILE__) . '/tasks/setup.php';
 
